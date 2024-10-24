0b28c3bba2e792f16401dfc3d1992d74e9b0de4c
Samuel Profeta <134728703+Samumuel@users.noreply.github.com>
2024-10-24 20:49:30 -0300

    Update Industrial.php

--- atividadesinterface/energia/modelo/Industrial.php
+++ atividadesinterface/energia/modelo/Industrial.php
@@ -11,17 +11,11 @@
         return $valorIndustrial;
     }
 
-        /**
-         * Get the value of consumoDiario
-         */
         public function getConsumoDiario(): float
         {
                 return $this->consumoDiario;
         }
 
-        /**
-         * Set the value of consumoDiario
-         */
         public function setConsumoDiario(float $consumoDiario): self
         {
                 $this->consumoDiario = $consumoDiario;
